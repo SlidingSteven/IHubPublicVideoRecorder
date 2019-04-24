3a7c3124c1586c556c3dbc466097bdf775b3208d
Chris Tse <chris@chris-tse.com>
2019-04-24 00:41:06 -0500

    Fix form template and email sending logic

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import smtplib 
-from email.mime.multipart import MIMEMultipart 
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText 
 from email.mime.base import MIMEBase 
 from email import encoders 
@@ -32,9 +33,11 @@ def send():
 @app.route('/sendemail/', methods=['GET', 'POST'])
 def send_email2():
     if request.method == 'POST':
+        print('FORM DATA:', request.form)
+        print('FILES:', request.files)
 
         fromaddr = os.environ.get('EMAIL_ADDRESS')
-        toaddr = os.environ.get('EMAIL_ADDRESS')
+        toaddr = request.form['email']
 
         # instance of MIMEMultipart 
         msg = MIMEMultipart() 
@@ -55,21 +58,34 @@ def send_email2():
         msg.attach(MIMEText(body, 'plain')) 
 
         # open the file to be sent 
-        filename = "video.webm"
-        attachment = open(request.form['video'], "rb") 
+        # filename = "video.webm"
+        # print(request.form['video'])
+        # filename = request.files[0][0]
+        # attachment = request.files[0][1]
 
-        # instance of MIMEBase and named as p 
-        p = MIMEBase('application', 'octet-stream') 
+        video = request.files['video']
+        print(video.filename)
+        print(video)
+        part = MIMEApplication(
+            video.read(),
+            Name=video.filename
+        )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % video.filename
+        msg.attach(part)
 
-        # To change the payload into encoded form 
-        p.set_payload((attachment).read()) 
+        # # instance of MIMEBase and named as p 
+        # p = MIMEBase('application', 'octet-stream') 
 
-        # encode into base64 
-        encoders.encode_base64(p) 
-        p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+        # # To change the payload into encoded form 
+        # p.set_payload((attachment).read()) 
 
-        # attach the instance 'p' to instance 'msg' 
-        msg.attach(p) 
+        # # encode into base64 
+        # encoders.encode_base64(p) 
+        # p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+        # # attach the instance 'p' to instance 'msg' 
+        # msg.attach(p) 
 
         # creates SMTP session 
         s = smtplib.SMTP('smtp.gmail.com', 587) 
@@ -89,7 +105,7 @@ def send_email2():
         # terminating the session 
         s.quit() 
 
-        return render_template('index.html')
+        return redirect('/', code=200)
 
 if __name__ == "__main__":
     app.run(debug=True)
